0c5ca9159376f3c0bc0aea87d52432a8c64d96d0
namelessdeveloper <1924019307@qq.com>
2019-07-04 21:14:41 +0800

    new

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello word!";
+    }
+}
